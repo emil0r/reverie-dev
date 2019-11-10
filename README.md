f9e47be0e88627f33e18931f251cfac0098e7cf6
Emil Bengtsson <emil@emil0r.com>
2019-11-10 01:11:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# reverie-dev
+
+A library for dev related tasks for when developing for reverie.
+
+## Usage
+
+Add to dependancies for a reverie project under dev deps. Do not ship with code that is supposed to live in production.
+
+## License
+
+Copyright © 2019 Emil Bengtsson
+
+---
+
+Coram Deo
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject reverie-dev "0.1.0-SNAPSHOT"
+
+  :description "dev related tasks when developing for reverie"
+
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+
+  :repl-options {:init-ns reverie-dev.core})
